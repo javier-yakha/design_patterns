fce6ef2c6600026d702feaae94832070aef452f8
Txpert <29957106+Txpert@users.noreply.github.com>
2024-05-06 02:01:01 +0200

    Singleton

---
+++ Singelton/BasicSingleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Singelton
+{
+    public class BasicSingleton
+    {
+        // Static variable that holds a single instance of the class
+        private static readonly BasicSingleton instance = new BasicSingleton();
+
+        // Private constructor prevents any other class from instantiating
+        private BasicSingleton()
+        {
+            Console.WriteLine("The Singleton has been created!");
+        }
+
+        // Public static means of getting the instance of the Singleton
+        public static BasicSingleton GetInstance()
+        {
+            return instance;
+        }
+    }
+}
+
---
+++ Singelton/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Singelton;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Accessing the Singleton instance
+        BasicSingleton obj1 = BasicSingleton.GetInstance();
+        Console.ReadKey();
+    }
+}
+
