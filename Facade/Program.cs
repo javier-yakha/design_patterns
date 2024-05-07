ad99d29b143a4e75d55f7df8feadbc4343cc1ba3
Txpert <29957106+Txpert@users.noreply.github.com>
2024-05-07 13:11:59 +0200

    update

--- Facade/Program.cs
+++ Facade/Program.cs
@@ -4,32 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Creating an instance of the airport system and performing operations
-        AirportSystem airport = new AirportSystem();
-        airport.CheckLuggage();
-        airport.TransportLuggageToAirplane();
+        //// Creating an instance of the airport system and performing operations
+        //AirportSystem airport = new AirportSystem();
+        //airport.CheckLuggage();
+        //airport.TransportLuggageToAirplane();
 
-        // Creating an instance of the airplane and loading and unloading luggage
-        Airplane plane = new Airplane();
-        plane.PutLuggageIn();
-        plane.PutLuggageOut();
+        //// Creating an instance of the airplane and loading and unloading luggage
+        //Airplane plane = new Airplane();
+        //plane.PutLuggageIn();
+        //plane.PutLuggageOut();
 
-        // Creating an instance of the local transport company and transporting luggage
-        LocalTransportCompany company = new LocalTransportCompany();
-        company.PutLuggageIntoTruck();
-        company.TransportLuggageToHotel();
+        //// Creating an instance of the local transport company and transporting luggage
+        //LocalTransportCompany company = new LocalTransportCompany();
+        //company.PutLuggageIntoTruck();
+        //company.TransportLuggageToHotel();
 
-        // Creating an instance of the hotel and transporting luggage to the room
-        Hotel hotel = new Hotel();
-        hotel.TransportLuggageToRoom();
+        //// Creating an instance of the hotel and transporting luggage to the room
+        //Hotel hotel = new Hotel();
+        //hotel.TransportLuggageToRoom();
 
-        // Wait for a key press to close the console window
-        Console.ReadKey();
-
-
-        //FacadeLuggageManagement bookingSystem = new FacadeLuggageManagement();
-        //bookingSystem.SendLuggage();
+        //// Wait for a key press to close the console window
         //Console.ReadKey();
+
+
+        FacadeLuggageManagement bookingSystem = new FacadeLuggageManagement();
+        bookingSystem.SendLuggage();
+        Console.ReadKey();
     }
 }
 
