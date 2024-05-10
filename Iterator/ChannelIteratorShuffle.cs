998f1ee793f24ece10f539f0212ab79c501a8212
Javier <javier.iff.dev@proton.me>
2024-05-10 13:48:22 +0200

    Shuffle iterator using fisher-yates

--- Iterator/ChannelIteratorShuffle.cs
+++ Iterator/ChannelIteratorShuffle.cs
@@ -3,18 +3,25 @@
 public class ChannelIteratorShuffle : IChannelIterator
 {
     private List<Channel> channels;
+    private int currentPosition = 0;
     public ChannelIteratorShuffle(List<Channel> channels)
     {
+        Random r = new Random();
+        // Fisher-Yates Shuffle
+        for (int i = channels.Count - 1; i > 0; i--)
+        {
+            int j = r.Next(0, i + 1);
+            // Tuple swap
+            (channels[i], channels[j]) = (channels[j], channels[i]);
+        }
         this.channels = channels;
     }
     public bool HasNext()
     {
-        return true;
+        return currentPosition < channels.Count;
     }
     public Channel Next()
     {
-        Random r = new Random();
-        int randomIndex = r.Next(channels.Count);
-        return channels[randomIndex];
+        return channels[currentPosition++];
     }
 }
